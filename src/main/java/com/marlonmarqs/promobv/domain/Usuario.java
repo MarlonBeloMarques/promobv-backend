c5c2c9d878c470cebc8e4acb23f8e4a7fd6bf106
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-01 19:34:31 -0300

    BD H2, create automatic base dados

--- src/main/java/com/marlonmarqs/promobv/domain/Usuario.java
+++ src/main/java/com/marlonmarqs/promobv/domain/Usuario.java
@@ -3,9 +3,17 @@ package com.marlonmarqs.promobv.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String apelido;
