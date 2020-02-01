6924b1047c9db885a5959cc4047a14c0f17151f4
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-01 15:42:52 -0300

    modelo conceitual init

---
+++ src/main/java/com/marlonmarqs/promobv/PromoBvApplication.java
@@ -0,0 +1,13 @@
+package com.marlonmarqs.promobv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoBvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoBvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlonmarqs/promobv/PromoBvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlonmarqs.promobv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoBvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
