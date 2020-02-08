57b9d71a8751b4663b10b688d968c27926453f54
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-08 11:37:28 -0300

    create notificacao and associations

---
+++ src/main/java/com/marlonmarqs/promobv/domain/Notificacao.java
@@ -0,0 +1,88 @@
+package com.marlonmarqs.promobv.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Notificacao implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private Date data;
+	private Date hora;
+	
+	@ManyToOne
+	@JoinColumn(name="promocao_id")
+	private Promocao promocao;
+	
+	private Notificacao() {
+		
+	}
+
+	public Notificacao(Integer id, Date data, Date hora) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.hora = hora;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Date getHora() {
+		return hora;
+	}
+
+	public void setHora(Date hora) {
+		this.hora = hora;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Notificacao other = (Notificacao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
