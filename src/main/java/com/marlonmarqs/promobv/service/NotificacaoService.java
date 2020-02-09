9b9787d7eea2f3c215d399a10fac96a3463c022a
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-09 01:20:40 -0300

    update populating notifications + create service

--- src/main/java/com/marlonmarqs/promobv/service/NotificacaoService.java
+++ src/main/java/com/marlonmarqs/promobv/service/NotificacaoService.java
@@ -1,5 +1,11 @@
 package com.marlonmarqs.promobv.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marlonmarqs.promobv.repository.NotificacaoRepository;
+
 public class NotificacaoService {
 
+	@Autowired
+	private NotificacaoRepository repo;
 }
