409a350ef78de63b07f1e0ec77efc57c958fe5db
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-09 12:24:36 -0300

    find promocao by categoria

--- src/main/java/com/marlonmarqs/promobv/resources/PromocaoResource.java
+++ src/main/java/com/marlonmarqs/promobv/resources/PromocaoResource.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marlonmarqs.promobv.domain.Notificacao;
 import com.marlonmarqs.promobv.domain.Promocao;
 import com.marlonmarqs.promobv.service.PromocaoService;
 
@@ -36,5 +39,16 @@ public class PromocaoResource {
 	public ResponseEntity<?> findAllUser(@PathVariable Integer id) {
 		List<Promocao> obj = service.findAllUser(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/categoria/{id}", method=RequestMethod.GET) 
+	public ResponseEntity<Page<Promocao>> findAll(
+			@PathVariable Integer id,
+			@RequestParam(value="page", defaultValue="0")Integer page, // valor padrão 
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="id")String orderBy, 
+			@RequestParam(value="direction", defaultValue="DESC")String direction) { 
+		Page<Promocao> list = service.findPage(id, page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list); 
 	}
 }
--- src/main/java/com/marlonmarqs/promobv/service/PromocaoService.java
+++ src/main/java/com/marlonmarqs/promobv/service/PromocaoService.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.marlonmarqs.promobv.domain.Categoria;
 import com.marlonmarqs.promobv.domain.Promocao;
 import com.marlonmarqs.promobv.repository.PromocaoRepository;
 import com.marlonmarqs.promobv.service.exceptions.ObjectNotFoundException;
@@ -19,6 +23,9 @@ public class PromocaoService {
 	@Autowired
 	private UsuarioService userService;
 
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Optional<Promocao> find(Integer id) {
 		Optional<Promocao> obj = repo.findById(id);
 		
@@ -36,5 +43,12 @@ public class PromocaoService {
 	public List<Promocao> findAllUser(Integer idUser) {
 		List<Promocao> objs = repo.findByUsuario(userService.find(idUser));
 		return objs;
+	}
+	
+	public Page<Promocao> findPage(Integer idCat, Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Optional<Categoria> cat = categoriaService.find(idCat);
+		return repo.findByCategoria(cat, pageRequest);
 	}
 }
