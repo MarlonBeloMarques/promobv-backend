69746d373a653143f5df9e37ca1b835f524e4bc9
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-19 15:46:33 -0300

    PUT Usuario and UsuarioDTO

--- src/main/java/com/marlonmarqs/promobv/service/UsuarioService.java
+++ src/main/java/com/marlonmarqs/promobv/service/UsuarioService.java
@@ -1,11 +1,13 @@
 package com.marlonmarqs.promobv.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marlonmarqs.promobv.domain.Usuario;
+import com.marlonmarqs.promobv.dto.UsuarioDTO;
 import com.marlonmarqs.promobv.repository.UsuarioRepository;
 import com.marlonmarqs.promobv.service.exceptions.ObjectNotFoundException;
 
@@ -22,5 +24,24 @@ public class UsuarioService {
 				+ ", Tipo: " + Usuario.class.getName()));
 		
 		return obj;
+	}
+	
+	public List<Usuario> findAll() {
+		return repo.findAll();
+	}
+	
+	public Usuario update(Usuario obj) {
+		Optional<Usuario> newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj.get());
+	}
+	
+	public Usuario fromDTO(UsuarioDTO objDto) {
+		return new Usuario(objDto.getId(), objDto.getNome(), objDto.getEmail(), null);
+	}
+	
+	private void updateData(Optional<Usuario> newObj, Usuario obj) {
+		newObj.get().setNome(obj.getNome());
+		newObj.get().setEmail(obj.getEmail());
 	}
 }
