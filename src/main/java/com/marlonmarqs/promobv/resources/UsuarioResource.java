c37145d6b3a458c594e5f88b4506df83d295f745
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-19 17:15:26 -0300

    delete User

--- src/main/java/com/marlonmarqs/promobv/resources/UsuarioResource.java
+++ src/main/java/com/marlonmarqs/promobv/resources/UsuarioResource.java
@@ -48,4 +48,10 @@ public class UsuarioResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) { 
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/marlonmarqs/promobv/service/UsuarioService.java
+++ src/main/java/com/marlonmarqs/promobv/service/UsuarioService.java
@@ -6,11 +6,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marlonmarqs.promobv.domain.Categoria;
 import com.marlonmarqs.promobv.domain.Usuario;
 import com.marlonmarqs.promobv.domain.enums.TipoPerfil;
 import com.marlonmarqs.promobv.dto.UsuarioDTO;
 import com.marlonmarqs.promobv.dto.UsuarioNewDTO;
 import com.marlonmarqs.promobv.repository.UsuarioRepository;
+import com.marlonmarqs.promobv.service.exceptions.DataIntegrityException;
 import com.marlonmarqs.promobv.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -35,6 +37,16 @@ public class UsuarioService {
 	public Usuario insert(Usuario obj) {
 		obj.setId(null);
 		return obj = repo.save(obj);
+	}
+	
+	public void delete(Integer id) throws DataIntegrityException {
+		Optional<Usuario> obj = find(id);
+
+		if(obj.get().getPromocoes().isEmpty()) {
+			repo.deleteById(id);
+		} else {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");	
+		}
 	}
 	
 	public Usuario update(Usuario obj) {
