7cbf53c6e59a63b40a06855c51ac7203b8623940
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-18 21:59:22 -0300

    insert categoria

--- src/main/java/com/marlonmarqs/promobv/resources/CategoriaResource.java
+++ src/main/java/com/marlonmarqs/promobv/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.marlonmarqs.promobv.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marlonmarqs.promobv.domain.Categoria;
 import com.marlonmarqs.promobv.service.CategoriaService;
@@ -31,5 +34,13 @@ public class CategoriaResource {
 	public ResponseEntity<?> findAll() {
 		List<Categoria> objs = service.findAll();
 		return ResponseEntity.ok().body(objs);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) { // @RequestBody = faz o json ser convertido para objeto java
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri(); // enumerar de forma crescente o id do uri
+		return ResponseEntity.created(uri).build();
 	}
 }
