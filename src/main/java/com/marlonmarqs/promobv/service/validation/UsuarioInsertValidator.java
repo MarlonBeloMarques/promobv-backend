8245afc6f027d09990d74d8a71ad0eab761d82bc
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-02-19 18:57:27 -0300

    validation: insert existing email

---
+++ src/main/java/com/marlonmarqs/promobv/service/validation/UsuarioInsertValidator.java
@@ -0,0 +1,39 @@
+package com.marlonmarqs.promobv.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marlonmarqs.promobv.domain.Usuario;
+import com.marlonmarqs.promobv.dto.UsuarioNewDTO;
+import com.marlonmarqs.promobv.repository.UsuarioRepository;
+import com.marlonmarqs.promobv.resources.exceptions.FieldMessage;
+
+public class UsuarioInsertValidator implements ConstraintValidator<UsuarioInsert, UsuarioNewDTO> {
+	
+	@Autowired
+	private UsuarioRepository repo;
+	
+	@Override
+	public void initialize(UsuarioInsert ann) {
+		
+	}
+	
+	@Override
+	public boolean isValid(UsuarioNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		for(FieldMessage e : list) {
+			// lista de erros do framework, permite transportar a minha lista de erros para o framework
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+				.addConstraintViolation();
+		}
+		
+		return list.isEmpty();
+	}
+}
