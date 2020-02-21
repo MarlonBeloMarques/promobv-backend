bfa625fccbe21bdb6ee73777b970927b47b5262b
MarlonMarques <marlon.belohd@gmail.com>
2020-02-20 22:35:12 -0400

    insert and remove notification
    update remove notification

--- src/main/java/com/marlonmarqs/promobv/resources/NotificacaoResource.java
+++ src/main/java/com/marlonmarqs/promobv/resources/NotificacaoResource.java
@@ -1,15 +1,22 @@
 package com.marlonmarqs.promobv.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marlonmarqs.promobv.domain.Notificacao;
+import com.marlonmarqs.promobv.dto.NotificacaoNewDTO;
 import com.marlonmarqs.promobv.service.NotificacaoService;
 
 @RestController
@@ -29,6 +36,21 @@ public class NotificacaoResource {
 			@RequestParam(value="direction", defaultValue="DESC")String direction) { 
 		Page<Notificacao> list = service.findPage(id, page, linesPerPage, orderBy, direction);
 		return ResponseEntity.ok().body(list); 
+	}
+	
+	@RequestMapping(method=RequestMethod.POST) 
+	public ResponseEntity<Void> insert(@Valid @RequestBody NotificacaoNewDTO objDto) {	
+		Notificacao obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri(); // enumerar de forma crescente o id do uri
+		return ResponseEntity.created(uri).build(); 
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) { 
+		service.remove(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/marlonmarqs/promobv/service/NotificacaoService.java
+++ src/main/java/com/marlonmarqs/promobv/service/NotificacaoService.java
@@ -2,6 +2,7 @@ package com.marlonmarqs.promobv.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,7 +13,14 @@ import org.springframework.stereotype.Service;
 
 import com.marlonmarqs.promobv.domain.Notificacao;
 import com.marlonmarqs.promobv.domain.Promocao;
+import com.marlonmarqs.promobv.domain.Usuario;
+import com.marlonmarqs.promobv.domain.enums.TipoNotificacao;
+import com.marlonmarqs.promobv.dto.NotificacaoNewDTO;
 import com.marlonmarqs.promobv.repository.NotificacaoRepository;
+import com.marlonmarqs.promobv.repository.PromocaoRepository;
+import com.marlonmarqs.promobv.repository.UsuarioRepository;
+import com.marlonmarqs.promobv.service.exceptions.DataIntegrityException;
+import com.marlonmarqs.promobv.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class NotificacaoService {
@@ -21,7 +29,25 @@ public class NotificacaoService {
 	private NotificacaoRepository repo;
 	
 	@Autowired
+	private PromocaoRepository promocaoRepository;
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	@Autowired
 	private PromocaoService promocaoService;
+	
+	@Autowired
+	private UsuarioService userService;
+	
+	private Optional<Notificacao> find(Integer id) {
+		Optional<Notificacao> obj = repo.findById(id);
+		
+		obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+				+ ", Tipo: " + Notificacao.class.getName()));
+		
+		return obj;
+	}
 	
 	public Page<Notificacao> findPage(Integer idUser, Integer page, Integer linesPerPage, String orderBy, String direction){
 		List<Notificacao> objs = new ArrayList<>();
@@ -32,7 +58,28 @@ public class NotificacaoService {
 		for (Promocao promocao : promocoes) {
 		 	objs.addAll(repo.findByPromocao(promocao));
 		}
-		
+				
 		return new PageImpl<>(objs, pageRequest, objs.size());
+	}
+	
+	public Notificacao insert(Notificacao obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		userRepository.save(obj.getUsuario());
+		promocaoRepository.save(obj.getPromocao());
+		return obj;
+	}
+	
+	public void remove(Integer id) {
+		Optional<Notificacao> obj = find(id);
+		
+		repo.delete(obj.get());
+	}
+	
+	public Notificacao fromDTO(NotificacaoNewDTO objDto) {
+		Optional<Usuario> user = userService.find(objDto.getIdUsuario());
+		Optional<Promocao> promo = promocaoService.find(objDto.getIdPromocao());
+		
+		return new Notificacao(null, objDto.getData(), objDto.getHora(), promo.get(), user.get(), TipoNotificacao.toEnum(objDto.getTipo()));
 	}
 }
