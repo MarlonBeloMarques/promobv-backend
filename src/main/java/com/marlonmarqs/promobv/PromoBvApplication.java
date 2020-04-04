abcc80663ea9d4a6282493b089a76b17f9a646e4
MarlonMarques <marlon.belohd@gmail.com>
2020-04-03 20:06:09 -0400

    running :Saving first file to S3

--- src/main/java/com/marlonmarqs/promobv/PromoBvApplication.java
+++ src/main/java/com/marlonmarqs/promobv/PromoBvApplication.java
@@ -1,13 +1,17 @@
 package com.marlonmarqs.promobv;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marlonmarqs.promobv.service.S3Service;
+
 @SpringBootApplication
 public class PromoBvApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PromoBvApplication.class, args);
@@ -16,6 +20,7 @@ public class PromoBvApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		s3Service.uploadFile("/Users/marlonmarques/downloads/perfil.jpg");
 		
 	}
 
