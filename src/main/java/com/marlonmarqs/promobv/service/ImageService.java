9a69b4547cd7b96573170f785290f66f59dedda0
MarlonMarques <marlon.belohd@gmail.com>
2020-04-04 11:26:06 -0400

    Adjusting image size

--- src/main/java/com/marlonmarqs/promobv/service/ImageService.java
+++ src/main/java/com/marlonmarqs/promobv/service/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -56,4 +57,19 @@ public class ImageService {
 		}
 	}
 
+	// codigo especifico para recortar imagem
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth(); // descobre quem é o min, largura ou altura
+		return Scalr.crop(
+				sourceImg,
+				(sourceImg.getWidth()/2) - (min/2),
+				(sourceImg.getHeight()/2) - (min/2),
+				min,
+				min);
+	}
+
+	//redimensiona a imagem
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
+	}
 }
