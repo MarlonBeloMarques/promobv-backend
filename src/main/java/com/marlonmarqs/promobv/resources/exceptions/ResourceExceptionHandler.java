2776158a1ca4cd0fe83ba5dd5cc9a5a07e07ceca
MarlonMarques <marlon.belohd@gmail.com>
2020-04-04 10:07:14 -0400

    Handling exceptions properly

--- src/main/java/com/marlonmarqs/promobv/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/marlonmarqs/promobv/resources/exceptions/ResourceExceptionHandler.java
@@ -9,9 +9,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.marlonmarqs.promobv.service.exceptions.AuthorizationException;
 import com.marlonmarqs.promobv.service.exceptions.BusinessRuleException;
 import com.marlonmarqs.promobv.service.exceptions.DataIntegrityException;
+import com.marlonmarqs.promobv.service.exceptions.FileException;
 import com.marlonmarqs.promobv.service.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice //metodo padrão do controlleradvice
@@ -55,5 +59,34 @@ public class ResourceExceptionHandler {
 		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(FileException.class) // tratador de exceçao do tipo passado
+	public ResponseEntity<StandardError> file (FileException e, HttpServletRequest request){
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis()); // acesso negado
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AmazonServiceException.class) // tratador de exceçao do tipo passado
+	public ResponseEntity<StandardError> amazonService (AmazonServiceException e, HttpServletRequest request){
+
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode()); //pega o codigo http da excecao e transforma em httpstatus
+		StandardError err = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis()); // acesso negado
+		return ResponseEntity.status(code).body(err);
+	}
+
+	@ExceptionHandler(AmazonClientException.class) // tratador de exceçao do tipo passado
+	public ResponseEntity<StandardError> amazonClient (AmazonClientException e, HttpServletRequest request){
+
+		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis()); // acesso negado
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
+
+	@ExceptionHandler(AmazonS3Exception.class) // tratador de exceçao do tipo passado
+	public ResponseEntity<StandardError> amazonS3 (AmazonS3Exception e, HttpServletRequest request){
+
+		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis()); // acesso negado
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
 	}
 }
--- src/main/java/com/marlonmarqs/promobv/service/S3Service.java
+++ src/main/java/com/marlonmarqs/promobv/service/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.marlonmarqs.promobv.service.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -35,7 +36,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();// string correspondido ao tipo do arquivo enviado
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 	}
 	
@@ -47,7 +48,7 @@ public class S3Service {
 			s3client.putObject(bucketName, fileName, is, meta);
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
