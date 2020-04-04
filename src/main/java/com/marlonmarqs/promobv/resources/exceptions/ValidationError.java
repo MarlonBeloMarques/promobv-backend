08ec47c1c46c9898c0243d1bd6239ee2ce7e4f83
MarlonMarques <marlon.belohd@gmail.com>
2020-04-04 13:27:00 -0400

    Standardizing exception format

--- src/main/java/com/marlonmarqs/promobv/resources/exceptions/ValidationError.java
+++ src/main/java/com/marlonmarqs/promobv/resources/exceptions/ValidationError.java
@@ -8,14 +8,10 @@ public class ValidationError extends StandardError {
 
 	private List<FieldMessage> errors = new ArrayList<>();
 
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
+	public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+		super(timeStamp, status, error, message, path);
 	}
-
-	public List<FieldMessage> getErrors() {
-		return errors;
-	}
-
+	
 	public void addError(String fieldName, String messagem) {
 		errors.add(new FieldMessage(fieldName,messagem));
 	}
