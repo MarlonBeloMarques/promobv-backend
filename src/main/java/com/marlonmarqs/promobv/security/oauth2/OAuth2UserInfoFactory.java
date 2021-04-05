5d079227719c22a505d320fdf3c0cc5a4c3a2b26
MarlonMarques <marlon.belohd@gmail.com>
2021-04-05 07:55:15 -0400

    add user information via oauth2

---
+++ src/main/java/com/marlonmarqs/promobv/security/oauth2/OAuth2UserInfoFactory.java
@@ -0,0 +1,19 @@
+package com.marlonmarqs.promobv.security.oauth2;
+
+import com.marlonmarqs.promobv.domain.enums.AuthProvider;
+import com.marlonmarqs.promobv.security.exceptions.OAuth2AuthenticationProcessingException;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        if(registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
+        } else if(registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
+            return new FacebookOAuth2UserInfo(attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Desculpe! O acesso com " + registrationId + " ainda não é compatível.");
+        }
+    }
+}
