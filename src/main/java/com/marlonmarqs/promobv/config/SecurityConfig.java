c608e43789b5cfc06675493f6cc2ac1f823277f6
MarlonMarques <marlon.belohd@gmail.com>
2021-05-08 11:29:03 -0400

    swagger documentation

--- src/main/java/com/marlonmarqs/promobv/config/SecurityConfig.java
+++ src/main/java/com/marlonmarqs/promobv/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -74,6 +75,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				"/usuarios",
 				"/usuarios/picture"
 		};
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+				"/swagger-ui.html", "/webjars/**");
+	}
 
 	@Bean(BeanIds.AUTHENTICATION_MANAGER)
 	@Override
